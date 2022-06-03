964348e43e68dd690a0d3e9d94b4c1ad14dd7ebf
Mr. Automation <31755155+Floogen@users.noreply.github.com>
2022-06-03 08:15:21 -0500

    Working SF API integration

---
+++ MysticalBuildings/Framework/Managers/ApiManager.cs
@@ -0,0 +1,35 @@
+﻿using MysticalBuildings.Framework.Interfaces;
+using StardewModdingAPI;
+
+namespace MysticalBuildings.Framework.Managers
+{
+    internal class ApiManager
+    {
+        private IMonitor _monitor;
+        private ISolidFoundationsApi _solidFoundationsApi;
+
+        public ApiManager(IMonitor monitor)
+        {
+            _monitor = monitor;
+        }
+
+        internal bool HookIntoSolidFoundations(IModHelper helper)
+        {
+            _solidFoundationsApi = helper.ModRegistry.GetApi<ISolidFoundationsApi>("PeacefulEnd.SolidFoundations");
+
+            if (_solidFoundationsApi is null)
+            {
+                _monitor.Log("Failed to hook into PeacefulEnd.SolidFoundations.", LogLevel.Error);
+                return false;
+            }
+
+            _monitor.Log("Successfully hooked into PeacefulEnd.SolidFoundations.", LogLevel.Debug);
+            return true;
+        }
+
+        public ISolidFoundationsApi GetSolidFoundationsApi()
+        {
+            return _solidFoundationsApi;
+        }
+    }
+}
--- MysticalBuildings/MysticalBuildings.cs
+++ MysticalBuildings/MysticalBuildings.cs
@@ -1,13 +1,46 @@
-﻿using StardewModdingAPI;
+﻿using MysticalBuildings.Framework.Interfaces;
+using MysticalBuildings.Framework.Managers;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
 using System;
 
 namespace MysticalBuildings
 {
     public class MysticalBuildings : Mod
     {
+        // Shared static helpers
+        internal static IMonitor monitor;
+        internal static IModHelper modHelper;
+
+        // Managers
+        internal static ApiManager apiManager;
+
         public override void Entry(IModHelper helper)
         {
-            throw new NotImplementedException();
+            // Set up the monitor, helper and multiplayer
+            monitor = Monitor;
+            modHelper = helper;
+
+            // Set up the managers
+            apiManager = new ApiManager(monitor);
+
+            // Hook into required events
+            helper.Events.GameLoop.GameLaunched += OnGameLaunched;
+        }
+
+        private void OnGameLaunched(object sender, GameLaunchedEventArgs e)
+        {
+            // Hook into the APIs we utilize
+            if (Helper.ModRegistry.IsLoaded("PeacefulEnd.SolidFoundations") && apiManager.HookIntoSolidFoundations(Helper))
+            {
+                var solidFoundationsApi = apiManager.GetSolidFoundationsApi();
+                solidFoundationsApi.BroadcastSpecialActionTriggered += OnBroadcastSpecialActionTriggered;
+            }
+        }
+
+        private void OnBroadcastSpecialActionTriggered(object sender, ISolidFoundationsApi.BroadcastEventArgs e)
+        {
+            Monitor.Log("HERE", LogLevel.Debug);
         }
     }
 }
