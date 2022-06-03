89cf3d6850601c6c2f0ce588a67497bfb903e4ae
Mr. Automation <31755155+Floogen@users.noreply.github.com>
2022-06-03 08:08:08 -0500

    Start of project

---
+++ MysticalBuildings/MysticalBuildings.cs
@@ -0,0 +1,13 @@
+﻿using StardewModdingAPI;
+using System;
+
+namespace MysticalBuildings
+{
+    public class MysticalBuildings : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
